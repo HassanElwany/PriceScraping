1ef024607209b192a019b1284905275409e8213c
HassanElwany <elwany_hassan@hotmail.com>
2023-05-16 23:10:39 +0300

    initial commit

---
+++ amazon.py
@@ -0,0 +1,17 @@
+from requests_html import HTMLSession
+
+s = HTMLSession()
+
+products = ['B088CZ8CYG', 'B097MQS598',
+            'B0B4JTKF6T', 'B0BYNDL5VJ', 'B09SFPJS27', 'B082M7TM9P']
+
+for product in products:
+
+    r = s.get(f'https://www.amazon.sa/-/en/gp/product/{product}/')
+
+    r.html.render(sleep=1)
+
+    price = r.html.find('.a-offscreen')[0].text.replace('SAR', '')
+    title = r.html.find('#productTitle')[0].text
+
+    print(title, price)
